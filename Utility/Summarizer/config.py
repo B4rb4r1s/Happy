6789a36c03c74032d09e7c50347dc40a673b44cf
B4rb4r1s <vgokarev@mail.ru>
2025-03-31 13:37:42 +0300

    all models

--- Utility/Summarizer/config.py
+++ Utility/Summarizer/config.py
@@ -5,7 +5,7 @@ DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 # DEVICE = "cpu"
 
 SUMMARY_MODELS = [
-    # './Happy/Models/Summary/csebuetnlp--mT5_multilingual_XLSum',
+    './Happy/Models/Summary/csebuetnlp--mT5_multilingual_XLSum',
     './Happy/Models/Summary/IlyaGusev--mbart_ru_sum_gazeta',
     './Happy/Models/Summary/IlyaGusev--rut5_base_sum_gazeta',
     './Happy/Models/Summary/utrobinmv--t5_summary_en_ru_zh_base_2048'
